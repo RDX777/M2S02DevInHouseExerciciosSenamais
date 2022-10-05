a5f0db1e23f35e02be2aa2ea4003caae1e74e993
Anderson Tome <anderson_tome@outlook.com.br>
2022-10-04 22:43:58 -0300

    Exercicio 2

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,30 @@
+const { v4: uuidv4 } = require('uuid')
+
 const express = require('express')
 const app = express()
+
 app.use(express.json())
 
-const PORT = process.env.Port || 3333
+const PORTA = 3333
 
-app.listen(PORT, () => {
-    console.log("Servidor iniciado na porta: " + PORT)
+const DADOS = []
+
+app.get("/pizzas", (request, response) => {
+  response.status(200).json(DADOS)
+})
+
+app.post("/pizzas", (request, response) => {
+  const pizza = {
+    id : uuidv4(),
+    name : request.body.name,
+    description : request.body.description,
+    price : request.body.price,
+    ingredients : request.body.ingredients,
+  }
+  DADOS.push(pizza)
+  response.status(201).json(pizza)
+})
+
+app.listen(PORTA, () => {
+    console.log("Servidor iniciado na porta: " + PORTA)
 })
